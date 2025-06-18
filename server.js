f5785d81d2e5d6fef8d80d8e2cb4a4666f48c500
東信こころの相談室 <toshinkokoro@gmail.com>
2025-06-18 14:33:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,3 +25,38 @@ io.on("connection", (socket) => {
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+// 🔽 静的ファイルを公開するフォルダ（public）を指定
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("ball-control", (data) => {
+    socket.broadcast.emit("ball-control", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+// 🔽 任意: / にアクセスされたら public-client.html にリダイレクトする
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "public-client.html"));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
